aab520633a341efefd722810764019c05f67239f
dj12589 <daria.jammal@gmail.com>
2018-10-30 21:07:02 -0700

    Complete Bare Minimum Requirements

--- src/solvers.js
+++ src/solvers.js
@@ -70,42 +70,103 @@ window.findNRooksSolution = function(n) {
   return solution;
 };
 
+
+
+// // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+// window.countNRooksSolutions = function(n) {
+//   //set an empty object to feed the board, since the board needs to take in an obj
+//   var obj = {};
+//   //set a property on the object for n, since this the format it said to pass to the new board
+//   obj.n = n;
+//   //pass the object to the new board
+//   board = new Board(obj);
+//   //then access the rows of the board
+//   var boardRows = board.rows();
+//   //this was given here; since we're returning a solution, set it to be undefined
+//   var solutionCount = 0;
+
+//   //set a recursive function that takes in a board and a specific row
+//   var recursiveRooks = function(board, row) {
+//   //if that row is the nth row
+//   if (row === n) {
+//     //set the solution equal to the stringified board rows
+//     solutionCount += 1;
+//     return solutionCount;
+//   }
+//   //otherwise, loop through the rows of the board, starting at the current row
+//   for (var i = row; i < boardRows.length; i++) {
+//     //set the current row equal to a variable called internal row
+//     var internalRow = boardRows[i];
+//     //loop through the rows again so you can access a corresponding column
+//     for (var j = 0; j < internalRow.length; j++) {
+//       //toggle the piece with the current row and current column
+//       board.togglePiece(i, j);
+//       //if the board has any rooks conflicts
+//         if (!board.hasAnyRooksConflicts()) {
+//          //go through this function again on the next row
+//           recursiveRooks(board, row + 1)
+//         }      
+//       //if there isn't a solution and the row is still less than the max rows
+//         //toggle the piece back
+//         board.togglePiece(i, j)
+    
+//       if (j === internalRow.length - 1) {
+//         return;
+//       }
+//     }
+//   }
+// }
+//   //start the function at the first row and the board
+//   recursiveRooks(board, 0);
+//   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+//   return solutionCount;
+// };
+//
+
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  //set an empty object to feed the board, since the board needs to take in an obj
   var obj = {};
-  //set a property on the object for n, since this the format it said to pass to the new board
   obj.n = n;
-  //pass the object to the new board
-  board = new Board(obj);
-  //then access the rows of the board
+  var board = new Board(obj);
   var boardRows = board.rows();
-  //this was given here; since we're returning a solution, set it to be undefined
   var solutionCount = 0;
+  var recursiveRooks = function(board, row, prevCol) {
+    var previousColumn = [].concat(prevCol);
+    if (row === n ) {
+      solutionCount++;
+      return;
+    }
+    
+    for (var i = row; i < boardRows.length; i++) {
+      var internalRow = boardRows[i];
 
-  //set a recursive function that takes in a board and a specific row
-  var recursiveRooks = function(board, row) {
-  //if that row is the nth row
-  if (row === n) {
-    //set the solution equal to the stringified board rows
-    solutionCount += 1;
-    return solutionCount;
-  }
-  //otherwise, loop through the rows of the board, starting at the current row
-  for (var i = row; i < boardRows.length; i++) {
-    //set the current row equal to a variable called internal row
-    var internalRow = boardRows[i];
-    //loop through the rows again so you can access a corresponding column
-    for (var j = 0; j < internalRow.length; j++) {
-      //toggle the piece with the current row and current column
-      board.togglePiece(i, j);
-      //if the board has any rooks conflicts
+      for (var j = 0; j < internalRow.length; j++) {
+        
+        if (i === 0) {
+          board.togglePiece(i,j);
+          var nexColumns = previousColumn.concat(j);
+          debugger;
 
-        if (!board.hasAnyRooksConflicts()) {
-         //go through this function again on the next row
-          recursiveRooks(board, row + 1)
+          recursiveRooks(board, row + 1, nexColumns);
         }
-      
+
+        board.togglePiece(i, j);
+        if (!previousColumn.includes(j)) {
+          if (!board.hasAnyRooksConflicts()) {
+            var nexColumns = previousColumn.concat(j);
+            recursiveRooks(board, row + 1, nexColumns);
+          }
+        }
+        board.togglePiece(i, j);
+        if (j === board.rows().length - 1) {
+          return;
+        }
+      }
+    }
+  };
+  recursiveRooks(board, 0, []);
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  return solutionCount;
       //if there isn't a solution and the row is still less than the max rows
         //toggle the piece back
         board.togglePiece(i, j)
@@ -113,11 +174,9 @@ window.countNRooksSolutions = function(n) {
       if (j === internalRow.length - 1) {
         return;
       }
-    }
-  }
-}
+    
   //start the function at the first row and the board
-  recursiveRooks(board, 0);
+  recursiveRooks(board, 0, []);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
